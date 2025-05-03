624d15a8967fce2188be7adb503508aad93feee1
Progga-Roy <proggaroy2000@gmail.com>
2025-05-03 14:06:06 +0600

    access a div method from the MethodCalling class

--- src/test/java/first/practice/MethodCalling.java
+++ src/test/java/first/practice/MethodCalling.java
@@ -3,10 +3,16 @@ package first.practice;
 import static first.practice.MethodExample.sub;
 
 public class MethodCalling {
+    public void div(int a ,int b){
+        int division = a/b;
+        System.out.println("Div : " + division);
+    }
     public static void main (String[] args){
         MethodExample obj = new MethodExample();
         obj.sum(11,23);
         obj.mul();
         sub(20,12);
+        MethodCalling object = new MethodCalling();
+        object.div(20,5);
     }
 }
